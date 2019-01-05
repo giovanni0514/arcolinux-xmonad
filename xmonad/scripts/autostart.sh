cb30b29888b79058938253a23508277729188320
Erik Dubois <erik.dubois@gmail.com>
2019-01-05 18:00:25 +0100

    run function and conky 4 seconds

--- xmonad/scripts/autostart.sh
+++ xmonad/scripts/autostart.sh
@@ -17,7 +17,7 @@ function run {
 #xrandr --output LVDS1 --mode 1366x768 --output DP3 --mode 1920x1080 --right-of LVDS1
 #xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
 
-(sleep 2; $HOME/.config/polybar/launch.sh) &
+(sleep 2; run $HOME/.config/polybar/launch.sh) &
 
 #change your keyboard if you need it
 #setxkbmap -layout be
@@ -28,7 +28,7 @@ xsetroot -cursor_name left_ptr &
 #Some ways to set your wallpaper besides variety or nitrogen
 feh --bg-scale ~/.xmonad/wall.jpg &
 #start the conky to learn the shortcuts
-(sleep 2; conky -c $HOME/.xmonad/scripts/system-overview) &
+(sleep 4; conky -c $HOME/.xmonad/scripts/system-overview) &
 
 #starting utility applications at boot time
 run variety &
